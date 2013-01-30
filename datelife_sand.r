6cc2fe61b2e096fc99e2fd1f5984ea2ed2ffaea4
schamberlain <myrmecocystus@gmail.com>
2013-01-30 09:13:13 -0700

    pretty much working code now for name checking

--- datelife_sand.r
+++ datelife_sand.r
@@ -1,36 +1,54 @@
 # Including TNRS in Datelife
-install_github("taxize_", "ropensci", ref="release")
-library(taxize)
 
-# testing with one tree
-library(ape); library(stringr)
-load("/Users/scottmac2/phyloorchard/pkg/branches/data/AlfaroEtAl2009.rda")
-# tree <- read.tree(file="~/github/sac/datelife/data/alfaro2009.rda")
-alfarotree <- AlfaroEtAl2009_tree
-alfarotree_tips <- str_replace_all(alfarotree$tip.label, "_", " ")
-out <- tnrs(alfarotree_tips, getpost="POST")[,1:5] # get data
-
-system.time(out <- tnrs(alfarotree_tips, getpost="POST")[,1:5])
-system.time(out <- tnrs(alfarotree_tips, getpost="POST", splitby=50, sleep=1)[,1:5])
-
-registerDoMC(cores=4)
-system.time( out <- llply(slice(alfarotree_tips, 75), function(x) tnrs(x, getpost="POST")[,1:5], .parallel=TRUE)  )
-
-# check for exact matches
-library(doMC)
-registerDoMC(cores=4)
-
+#' Function to break up a vector by certain N sized chunks.
 #' 
+#' @import plyr
+#' @param input Input character vector
+#' @pararm by Number by which to split the input character vector
+slice <- function(input, by = 2) {
+	starts <- seq(1, length(input), by)
+	tt <- lapply(starts, function(y) input[y:(y + (by - 1))])
+	llply(tt, function(x) x[!is.na(x)])
+}
+	
+#' Function to check names on a species list or phylogeny.
+#' 
+#' @import taxize
 #' @param phylo Phylogeny object, class phylo
 #' @param source_ Source to match names to. One of ncbi, iPlant
 #' @param splitby Length of chunks by which to split species list
 #' @examples \dontrun{
-#' replacenames(alfarotree, source_="NCBI", splitby=100)
+#' # A phylogeny as input
+#' library(doMC)
+#' out <- checknames(obj=BergmannEtAl2012_tree, source_="NCBI", splitby=200)
+#' out <- checknames(obj=AlfaroEtAl2009_tree, source_="NCBI", splitby=100)
+#' out <- checknames(obj=EastmanEtAlUnpublished_tree, source_="NCBI", splitby=100)
+#' out <- checknames(obj=HeathEtAl2012_tree[[1]], source_="NCBI")
+#' out <- checknames(obj=JaffeEtAl2011_tree, source_="NCBI", splitby=100)
+#' out <- checknames(obj=Chaetodontidae2011[[1]][[1]], source_="NCBI", splitby=100)
+#' 
+#' # A character vector as input (of species names)
+#' out <- checknames(obj=AlfaroEtAl2009_tree$tip.label, source_="NCBI", splitby=50)
+#' out <- checknames(obj=AlfaroEtAl2009_tree$tip.label[1:20], source_="NCBI")
 #' }
-replacenames <- function(phylo, source_ = "NCBI", splitby = 100){
-	tree_tips <- str_replace_all(phylo$tip.label, "_", " ") # replace underscores
-	registerDoMC(cores=4)
-	out <- ldply(slice(tree_tips, splitby), function(x) tnrs(x, getpost="POST")[,1:4], .parallel=TRUE) # get TNRS data
+checknames <- function(obj, source_ = "NCBI", splitby = NULL)
+{	
+	if(class(obj)=="phylo"){
+		tree_tips <- str_replace_all(obj$tip.label, "_", " ") # replace underscores
+		orig_tips <- obj$tip.label
+	} else
+	{
+		tree_tips <- str_replace_all(obj, "_", " ") # replace underscores
+		orig_tips <- tree_tips
+	}
+	
+	if(is.null(splitby)){
+		out <- tnrs(tree_tips, getpost="POST")[,1:4] # get TNRS data
+	} else
+	{
+		registerDoMC(cores=4)
+		out <- ldply(slice(tree_tips, splitby), function(x) tnrs(x, getpost="POST")[,1:4], .parallel=TRUE) # get TNRS data
+	}
 	
 	temp <- out[out$sourceId %in% source_, ]
 	
@@ -52,22 +70,64 @@ replacenames <- function(phylo, source_ = "NCBI", splitby = 100){
 	temp22$submittedName <- str_replace_all(temp22$submittedName, " ", "_")
 	temp22$V1 <- str_replace_all(temp22$V1, " ", "_")
 	
-	notnrsmatch <- phylo$tip.label[!phylo$tip.label %in% as.character(temp22$submittedName)] # no match at all
+	notnrsmatch <- orig_tips[!orig_tips %in% as.character(temp22$submittedName)] # no match at all
 	temp33 <- rbind(temp22, data.frame(submittedName=notnrsmatch, V1=notnrsmatch)) # add notnrsmatches to data.frame
+	order_ <- sapply(orig_tips, function(x) match(x, temp33$submittedName), USE.NAMES=F)
+	temp3 <- temp33[order_,] # reorder data.frame to order of tip.labels
 	
-# 	order_ <- sapply(temp$sub, function(x) grep(x, temp22$submittedName))
+	# Keep names that match genera, but not those that don't match genera
+	foooo <- function(x){ 
+		if(as.character(x$old) %in% as.character(x$new)) { 0 } else { 1 }
+	}
+	df_ <- data.frame(old=orig_tips, new=temp3$V1)
+	df_2 <- ddply(df_, .(old), foooo)
+	df_2_merge <- merge(df_, df_2, by="old")
+	df_3 <- droplevels(df_2_merge[df_2_merge$V1 == 1, ])
 	
-	order_ <- sapply(phylo$tip.label, function(x) grep(x, as.character(temp33$submittedName)))
+	genusmatch <- function(x){
+		one <- str_split(x$old, "_")[[1]]
+		two <- str_split(x$new, "_")[[1]]
+		ifelse(one[[1]] %in% two[[1]], as.character(x$new), as.character(x$old))
+	}
+	asdf <- adply(df_3, 1, genusmatch) 
 	
-	temp3 <- temp33[order_,]
-	phylo$tip.label <- temp3$V1
-	return(phylo)
+	if(!nrow(asdf)==0){
+		order_2 <- sapply(temp3[temp3$submittedName %in% asdf$old, "submittedName"], function(x) match(x, as.character(asdf$old)), USE.NAMES=F)
+		asdf2 <- asdf[order_2,]
+		temp3[temp3$submittedName %in% asdf$old, "V1"] <- asdf2$V1
+	} else
+	{
+		temp3 <- temp3
+	}
+	
+	if(class(obj)=="phylo"){
+		obj$tip.label <- temp3$V1 # assign new names to tip.labels on phylo object
+		return(obj)
+	} else
+	{
+		obj <- temp3$V1
+		return(obj)
+	}
 }
 
-# parallelize = machine has 4 cores - may be able to add more cores
-# How much will we be able to parallelize taxosaurus calls?
-library(doMC)
-registerDoMC(cores=4)
+# Check species names against Taxosaurus
+# Run and write trees to directory with new file name, just appending "_new" to the end
+# Run the function replacenames across all trees
+# install_github("taxize_", "ropensci", ref="release")
+library(taxize); library(ape); library(stringr)
+# treefilenames <- c("AlfaroEtAl2009.rda","BergmannEtAl2012.rda","BinindaEmondsEtAl2007.rda",
+# 		"EastmanEtAlUnpublished.rda","HeathEtAl2012.rda","JaffeEtAl2011.rda","megaTree.rda",
+# 		"Oaks2011.rda","PyronWiens2011.rda","RaboskyEtAlUnpublished.rda","SantiniEtAl2009.rda",
+# 		"SmithEtAl2011.rda","TreeBase.rda","ZhangandWake2009.rda")
+treefilenames <- dir("/Users/scottmac2/phyloorchard3")
 
+# Load all trees into the workspace
+# CHANGE PATH!!!!
+llply(treefilenames, function(x) load(paste("/Users/scottmac2/phyloorchard3/",x,sep=""), .GlobalEnv))
 
-# 
+# Get tree names in the workspace
+trees <- sapply(treefilenames, function(x) str_replace(x, ".rda", "_tree"), USE.NAMES=F)
+
+# Run em all
+checknames_safe <- plyr::failwith(NULL, checknames)
+l_ply(trees, checknames_safe)
